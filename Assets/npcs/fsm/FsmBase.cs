dc31fa360202f44f6475942205a7da9c6484029c
japkirat <97636264+japkirat@users.noreply.github.com>
2024-01-27 14:20:17 -0700

    Revert "Merge branch 'main' into japkirat"
    This reverts commit 5974ce6b8752e8ebcfea94f7895c2bba4d065084, reversing
    changes made to db4f54f2b800892d4a04171d0bc9b199f2ba6a79.

--- Assets/npcs/fsm/FsmBase.cs
+++
@@ -1,6 +0,0 @@
-public interface StateBase
-{
-    public virtual void OnEnter(AgentBrain agent) {}
-    public virtual void OnExit(AgentBrain agent) {}
-    public virtual StateBase Think(AgentBrain agent) { return null; }
-}
