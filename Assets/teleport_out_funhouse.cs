812c92655ce302b2cb490f7cef291424188f2798
Vira <virawhelen@gmail.com>
2024-01-28 14:05:28 -0700

    assets added

--- Assets/teleport_out_funhouse.cs
+++ Assets/teleport_out_funhouse.cs
@@ -7,6 +7,7 @@ public class teleport_out_funhouse : MonoBehaviour
     // Start is called before the first frame update
 void OnTriggerEnter(Collider hit) {
 GameObject.Find("Player").transform.position = GameObject.Find("wheel_enter").transform.position;
+GameObject.Find("Player").transform.rotation = GameObject.Find("wheel_enter").transform.rotation;
 
 }
 
