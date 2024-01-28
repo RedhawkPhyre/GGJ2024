0361563d9495f9df46598f1f84a5a0126634ce53
japkirat <97636264+japkirat@users.noreply.github.com>
2024-01-27 20:51:06 -0700

    fixed fading

--- Assets/Dissipate.cs
+++ Assets/Dissipate.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Dissipate : MonoBehaviour
 {
-    public float delay = 10;
+    public float delay = 2.5f;
+    public float lifespan = 10;
+
+    private ParticleSystem.Particle[] particles;
+    private ParticleSystem system;
+    private float time;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, delay);
+        system =  gameObject.GetComponentInChildren<ParticleSystem>();
     }
 
-    // Update is called once per frame
+
     void Update()
-    { 
+    {
+        time += Time.deltaTime;
+
+        if (!system.isPaused && time > delay)
+        {
+            system.Pause();
+
+            particles = new ParticleSystem.Particle[system.particleCount];
+            system.GetParticles(particles);
+        }
+
+        if (particles != null)
+        {
+            for (int p = 0; p < particles.Length; p++)
+            {
+                Color color = particles[p].startColor;
+                color.a = ((lifespan - time + delay) / lifespan);
+
+                if (color.a <= 0) Destroy(gameObject);
+
+                particles[p].startColor = color;
+            }
+
+            system.SetParticles(particles, particles.Length);
+        }
     }
 }
