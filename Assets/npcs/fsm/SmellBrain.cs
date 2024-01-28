3d267b1a6459ba364f2c47fa2b0181e0556cfc63
Bailey Danyluk <baileydanyluk@gmail.com>
2024-01-28 10:32:53 -0700

    progress commit

--- Assets/npcs/fsm/SmellBrain.cs
+++ Assets/npcs/fsm/SmellBrain.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class SmellBrain : AgentBrain
 {
     public float patrol_change_time = 20.0f;
--- Assets/npcs/fsm/SmellFSM.cs
+++ Assets/npcs/fsm/SmellFSM.cs
@@ -2,12 +2,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace Smeller {
+    class Utils
+    {
+        public static Vector3 GetTargetPos(AgentBrain brain)
+        {
+            SightSense eyes = brain.agent.GetComponent<SightSense>();
+            HearSense ears = brain.agent.GetComponent<HearSense>();
+            SmellSense nose = brain.agent.GetComponent<SmellSense>();
+
+            float weight_sum = 0.0f;
+            Vector3 target = Vector3.zero;
+            Vector3 believed_position;
+            if (eyes.GetBelievedPosition(out believed_position))
+            {
+                target += 100.0f * believed_position;
+                weight_sum += 100.0f;
+            }
+
+            if (ears.GetBelievedPosition(out believed_position))
+            {
+                target += 3.0f * believed_position;
+                weight_sum += 3.0f;
+            }
+
+            if (nose.GetBelievedPosition(out believed_position))
+            {
+                target += 6.0f * believed_position;
+                weight_sum += 6.0f;
+            }
+            target /= weight_sum;
+            return target;
+        }
+    }
     public class Observe: StateBase
     {
         float time_entered = Time.time;
         public StateBase Think(AgentBrain brain)
         {
             SmellBrain agent = (SmellBrain)brain;
+
+            SightSense eyes = brain.agent.GetComponent<SightSense>();
+            HearSense ears = brain.agent.GetComponent<HearSense>();
+            SmellSense nose = brain.agent.GetComponent<SmellSense>();
+
+            Vector3 believed_position;
+            if (
+                eyes.GetBelievedPosition(out believed_position) ||
+                ears.GetBelievedPosition(out believed_position) ||
+                nose.GetBelievedPosition(out believed_position)
+            )
+            {
+                return new Chase();
+            }
 
             if (Time.time - time_entered >= agent.patrol_change_time)
             {
@@ -43,6 +89,8 @@ namespace Smeller {
 
         public StateBase Think(AgentBrain brain)
         {
+            HearSense ears = brain.agent.GetComponent<HearSense>();
+            SmellSense nose = brain.agent.GetComponent<SmellSense>();
             if (path_state == State.AtNode)
             {
                 path.RemoveAt(0);
@@ -56,10 +104,6 @@ namespace Smeller {
             else
             {
                 Vector3 direction = path[0] - brain.transform.position;
-                Debug.Log("Patrol");
-                Debug.Log(direction);
-                Debug.Log(path[0]);
-                Debug.Log(brain.transform.position);
                 brain.agent.GetComponent<AiController>().Walk(direction);
                 if (direction.magnitude <= 2.0f)
                 {
@@ -72,14 +116,63 @@ namespace Smeller {
 
     public class Chase: StateBase
     {
+        Vector3 target;
+        float time_at_target = -1.0f;
+        public void OnEnter(AgentBrain brain)
+        {
+            target = Utils.GetTargetPos(brain);
+        }
+
         public StateBase Think(AgentBrain brain)
         {
+            SightSense eyes = brain.agent.GetComponent<SightSense>();
+            HearSense ears = brain.agent.GetComponent<HearSense>();
+            SmellSense nose = brain.agent.GetComponent<SmellSense>();
+
+            float distance_to_target = (brain.agent.transform.position - target).magnitude;
+            Vector3 believed_position;
+            if (
+                distance_to_target <= 2.0f &&
+                !eyes.GetBelievedPosition(out believed_position) &&
+                !ears.GetBelievedPosition(out believed_position) &&
+                !nose.GetBelievedPosition(out believed_position)
+            ) {
+                if (time_at_target < 0.0f)
+                {
+                    time_at_target = Time.time;
+                } else if (Time.time - time_at_target >= 5.0f)
+                {
+                    return new Observe();
+                }
+            }
+
+            target += 5.0f * Utils.GetTargetPos(brain);
+            target /= 6.0f;
+
+            Vector3 direction = brain.agent.transform.position - target;
+            brain.agent.GetComponent<AiController>().Run(direction);
+
+            Vector3 player_pos;
+            if (eyes.GetBelievedPosition(out player_pos))
+            {
+                if ((player_pos - brain.agent.transform.position).magnitude <= 10.0f)
+                {
+                    return new Grab();
+                }
+            }
+
             return null;
         }
     }
 
     public class Grab: StateBase
     {
+        public void OnEnter(AgentBrain brain)
+        {
+            Vector3 direction = brain.agent.transform.position - SenseManager.GetSenseManager().sight_target.transform.position;
+            brain.agent.GetComponent<AiController>().Grab(direction);
+        }
+
         public StateBase Think(AgentBrain brain)
         {
             return null;
