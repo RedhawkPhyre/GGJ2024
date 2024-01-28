7bf84c09b7b6b2a41331868c3bbae451893782a2
japkirat <97636264+japkirat@users.noreply.github.com>
2024-01-27 22:54:04 -0700

    added balloon popping sound

--- Assets/Release Gas.cs
+++ Assets/Release Gas.cs
@@ -7,9 +7,13 @@ public class ReleaseGas : MonoBehaviour
 
     public Transform spawnpoint;
     public GameObject preFab;
+    public AudioSource popSound;
+    public AudioClip clip;
 
     private void OnTriggerEnter(Collider other)
     {
+        popSound.loop = false;
+        popSound.PlayOneShot(clip);
         Instantiate(preFab, spawnpoint.position, spawnpoint.rotation);
         Destroy(gameObject);
     }
